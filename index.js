39ba65f9cf6f4ff5793b76ca06f9791e68914338
radbrawler <radbrawler@gmail.com>
2017-02-03 23:30:52 +0530

    port number bug fix

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 3000));
+port = (process.env.PORT || 3000)
+app.set('port', port);
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
